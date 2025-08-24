6aa654cf95cbfeb041e31e355aaf76a87dbe127d
Kartik Tripathi <85053666+K9TX@users.noreply.github.com>
2025-08-24 09:46:52 +0530

    Update manage.py

--- backend/manage.py
+++ backend/manage.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
+
 import os
 import sys
 def main():
@@ -18,3 +18,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
